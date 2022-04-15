7ff7e13dd19a2100fbfcfb10a5ccc856d31c0ee4
ev-n-er <evgenyerkaev@gmail.com>
2022-04-16 00:20:37 +0700

    Heroku default port

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -14,10 +14,15 @@ func main() {
 	apiKey := os.Getenv("JARVIS_BOT_API_KEY")
 	serverUrl := os.Getenv("JARVIS_BOT_SERVER_URL")
 
-	if botServer, err := server.CreateNew(":8888", apiKey, serverUrl); err != nil {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8888"
+	}
+
+	if botServer, err := server.CreateNew(port, apiKey, serverUrl); err != nil {
 		log.Fatal(err)
 	} else {
-		log.Print("Starting listner")
+		log.Printf("Starting listner at port %s", port)
 		err = botServer.ListenAndServe()
 		if err != nil {
 			log.Fatal(err)
